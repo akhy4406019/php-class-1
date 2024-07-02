3ba9b9f716808fc8aec7a31f8e495da4a67a2a6f
akhy4406019 <149380498+akhy4406019@users.noreply.github.com>
2024-07-02 16:21:18 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+    // $x=23;
+    // $y=50;
+    // $result=$x+$y;
+    // echo $result;
+
+    // $sname="Akhy";
+    // $roll="13";
+    // echo $sname." ".$roll;
+
+
+    // function auto($x,$y){
+    //     $result=$x+$y;
+    //     return $result;
+    // }
+    // echo auto(55,12);
+
+    
+    function sub($x,$y){
+        $result=$x-$y;
+        return $result;
+    }
+    echo sub(55,12);
+
+?>
